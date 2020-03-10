4acd6ec5330451f2ac6baaee5fc413fa03a5ca9f
Jason Chow <jason@hato.co>
2020-03-10 08:20:05 +0000

    redirect enquire to register your interest

---
+++ pages/enquire/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="">
+  </div>
+</template>
+
+<script>
+export default {
+
+  middleware ({ store, redirect }) {
+    return redirect('/contact#register-interest')
+  },
+
+  head () {
+    return {
+      title: 'Enquire',
+      meta: [
+        // hid is used as unique identifier. Do not use `vmid` for it as it will not work
+        { hid: 'description', name: 'description', content: 'Ask a question or book a viewing. Comment or collaborate. We want to hear from you.' }
+      ]
+    }
+  },
+}
+</script>
+
+<style lang="stylus">
+</style>
