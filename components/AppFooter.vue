6fb7e2364ca3e035be6eedf44909a6b4561ae6c8
jason <jason@studiohato.com>
2020-03-10 18:34:37 +0000

    updated footer

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -5,10 +5,11 @@
         <div class="col-12 col-md-3 col-lg-3 mb-5">
           <h5>DESIGN DISTRICT</h5>
           <a target="_blank" rel="noreferrer" href="https://www.google.com/maps/search/The+Gateway+Pavilions,+Peninsula+Square,+Greenwich+Peninsula,+London+SE10+0SQ/@51.5003894,0.0041197,18.67z?hl=en">The Gateway Pavilions<br>Peninsula Square<br>Greenwich Peninsula<br>London SE10 0SQ</a><br><br>
-          <email :address="'hello@designdistrict.co.uk'"></email><br>
+          <email :address="'hello@designdistrict.co.uk'" /><br>
           <a class="">+44 (0)20 3981 2426</a><br>
         </div>
-        <div class="col-6 col-md-3 col-lg-3 mb-5">
+
+        <div class="col-6 col-md-3 col-lg-2 mb-5">
           <h5>Connect</h5>
           <a class="" target="_blank" rel="noreferrer" href="https://www.instagram.com/designdistrictlondon">Instagram</a><br>
           <a class="" target="_blank" rel="noreferrer" href="https://twitter.com/Design_District">Twitter</a><br>
@@ -16,7 +17,7 @@
           <a class="" target="_blank" rel="noreferrer" href="https://www.linkedin.com/company/design-district-london">LinkedIn</a>
         </div>
 
-        <div class="col-6 col-md-3 col-lg-3 mb-5">
+        <div class="col-6 col-md-3 col-lg-2 mb-5">
           <h5>Join</h5>
           <nuxt-link class="" to="/workspace">
             Work space
@@ -35,7 +36,7 @@
           </nuxt-link>
         </div>
 
-        <div class="col-6 col-md-3 col-lg-3 mb-5">
+        <div class="col-6 col-md-3 col-lg-2 mb-5">
           <h5>Legal</h5>
           <nuxt-link class="" to="/privacy-policy">
             Privacy Policy
@@ -50,20 +51,27 @@
           </nuxt-link>
         </div>
 
+        <div class="col-6 col-md-3 col-lg-1" />
+
+        <div class="col-6 col-md-3 col-lg-2 mb-5">
+          <img src="~/assets/images/logo-hor.svg" alt="Design District logo" style="" class="mb-4 w-75">
+          <img src="~/assets/images/gp-logo.svg" alt="Greenwich Peninsula logo" style="" class=" w-75 pr-5">
+        </div>
+
         <!-- <div class="col-12 d-block d-md-none" />
         <div class="col-6 col-md-3 col-lg-2 mb-5 ml-md-auto text-md-right text-left">
           <img src="~/assets/images/gp-logo.svg" alt="Greenwich Peninsula" class="w-100 w-md-75" style="max-width: 100px">
         </div> -->
       </div>
 
-      <div class="inner d-flex row align-items-end justify-content-start mt-2 pb-4">
+      <div class="inner d-flex row align-items-end justify-content-start mt-0 pb-4">
         <!-- <div class="p-3 mr-auto-mr-md-0-ml-md-auto-order-md-2 d-flex align-items-end">
           <img src="~/assets/images/logo-hor.svg" alt="Design District logo" style="max-width: 40px" class="mr-5">
           <img src="~/assets/images/gp-logo.svg" alt="Greenwich Peninsula logo" style="max-width: 100px">
         </div> -->
         <div class="col-6 col-md-2 pr-0 pr-md-5">
-          <img src="~/assets/images/logo-hor.svg" alt="Design District logo" style="" class="mb-4 w-75">
-          <img src="~/assets/images/gp-logo.svg" alt="Greenwich Peninsula logo" style="" class=" w-75 pr-5">
+          <!-- <img src="~/assets/images/logo-hor.svg" alt="Design District logo" style="" class="mb-4 w-75">
+          <img src="~/assets/images/gp-logo.svg" alt="Greenwich Peninsula logo" style="" class=" w-75 pr-5"> -->
         </div>
 
         <small class="col-12 col-md-6 text-left text-md-right ml-auto mt-2">
