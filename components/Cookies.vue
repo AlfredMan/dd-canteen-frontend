c9af18c09571e909b3ba8af9f740463cd74811c9
Jason <jason@hato.co>
2020-06-19 16:48:16 +0100

    updated cooke message style

--- components/Cookies.vue
+++ components/Cookies.vue
@@ -1,12 +1,12 @@
 <template lang="html">
   <div class="cookies-prompt bg-dark text-white p-5" v-if="!cookieActive">
-    <div class="">
+    <div class="container">
       <p>Like most websites, Design District uses cookies to help give you the best experience of our website. Please accept cookies for optimal performance. For more information, please read our <nuxt-link class="" to="/privacy-policy">
         Privacy Policy
       </nuxt-link> and <nuxt-link class="" to="/cookies-policy">
         Cookies Policy
       </nuxt-link>. </p>
-      <a class="btn btn-outline-primary text-primary" @click.prevent="accept">
+      <a class="btn btn-outline-secondary text-secondary" @click.prevent="accept">
         Accept and Continue
       </a>
     </div>
