9d8e83085ae8620a68a30c5b775434da05e23b1d
Jason <jason@hato.co>
2020-08-15 08:20:03 +0100

    check via document cookie

--- components/Cookies.vue
+++ components/Cookies.vue
@@ -1,5 +1,5 @@
 <template lang="html">
-  <div class="cookies-prompt bg-dark text-white p-5" v-if="!cookieActive">
+  <div class="cookies-prompt bg-dark text-white p-5" v-if="!cookieActive&&!forceActive">
     <div class="container">
       <p>Like most websites, Design District uses cookies to help give you the best experience of our website. Please accept cookies for optimal performance. For more information, please read our <nuxt-link class="" to="/privacy-policy">
         Privacy Policy
@@ -17,16 +17,17 @@
 export default {
   data () {
     return {
+      forceActive: false
     }
   },
   computed: {
     cookieActive () {
-      return this.$store.state.cookieActive || this.$cookies.get('DD_CP')
+      return this.$store.state.cookieActive || this.$cookies.get('DD_CP') || document.cookie.indexOf('DD_CP')>-1
     }
   },
   methods: {
     accept () {
-      this.cookieActive=true
+      this.forceActive=true
       this.$store.dispatch('COOKIE_ACCEPT')
     }
   }
