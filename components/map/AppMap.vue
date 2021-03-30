1d1dcab0c81bdaf7b3b9b7524de0712f82620a54
Alfred Man <alfalfman@gmail.com>
2021-03-30 09:55:41 +0100

    try adding window size detector

--- components/map/AppMap.vue
+++ components/map/AppMap.vue
@@ -6,25 +6,60 @@
 </template>
 
 <script>
-import AppMapPanel from '~/components/map/AppMapPanel'
-import AppMapRenderer from '~/components/map/AppMapRenderer'
+import AppMapPanel from "~/components/map/AppMapPanel";
+import AppMapRenderer from "~/components/map/AppMapRenderer";
 export default {
   components: {
     AppMapPanel,
     AppMapRenderer
+  },
+  data() {
+    return {
+      windowWidth: typeof window === "undefined" ? 0 : window.innerWidth
+    };
+  },
+  watch: {
+    windowWidth(newWidth, oldWidth) {
+      this.txt = `it changed to ${newWidth} from ${oldWidth}`;
+    }
+  },
+
+  mounted() {
+    this.$nextTick(() => {
+      if (typeof window === "undefined") {
+        return;
+      }
+      window.addEventListener("resize", this.onResize);
+    });
+  },
+
+  beforeDestroy() {
+    if (typeof window === "undefined") {
+      return;
+    }
+    window.removeEventListener("resize", this.onResize);
+  },
+
+  methods: {
+    onResize() {
+      if (typeof window === "undefined") {
+        return;
+      }
+      this.windowWidth = window.innerWidth;
+      console.log(this.windowWidth);
+    }
   }
-}
+};
 </script>
 
 <style lang="scss" scoped>
-
 .app-map-panel {
   @apply fixed top-0 left-0 bottom-0 overflow-y-auto w-full p-4;
   @apply bg-white;
   z-index: 999;
 }
 .app-map-renderer {
-  @apply fixed inset-0
+  @apply fixed inset-0;
 }
 
 @screen md {
@@ -33,7 +68,7 @@ export default {
     z-index: 999;
   }
   .app-map-renderer {
-    @apply fixed inset-0 pl-96
+    @apply fixed inset-0 pl-96;
   }
 }
 </style>
