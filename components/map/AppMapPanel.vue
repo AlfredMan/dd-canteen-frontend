6b34bea07cb0787f4732e9e7f92eb134cdab2c8b
Alfred Man <alfalfman@gmail.com>
2021-04-06 16:30:30 +0100

    added scroll top after collapse

--- components/map/AppMapPanel.vue
+++ components/map/AppMapPanel.vue
@@ -11,19 +11,20 @@
   > -->
   <!-- <div class="app-map-panel" ref="appMapPanel" v-on:scroll="onWindowScroll"> -->
   <div class="app-map-panel" ref="appMapPanel" style="transform:none!important">
-
     <!-- force disabled as below dragging is triggered by unknown reasons -->
 
     <div
       v-if="isMobilePortrait"
       class="sticky z-30 top-0 panel-draggable-handle-container w-full  bg-opacity-0 bg-gray-200 flex justify-center "
-       v-bind:class="{'h-full': isDraggableInfoPanelCollapsed, 'h-14': !isDraggableInfoPanelCollapsed }"
+      v-bind:class="{
+        'h-full': isDraggableInfoPanelCollapsed,
+        'h-14': !isDraggableInfoPanelCollapsed
+      }"
       @touchstart="dragTriggerTouchStart"
       @touchend="dragTriggerTouchEnd"
       @mousedown="dragTriggerTouchStart"
       @mouseup="dragTriggerTouchEnd"
     >
-
       <div class="absolute top-2 w-1/4 rounded-full h-1 bg-gray-800"></div>
       <!-- <div v-if="canGoBack" class="absolute left-0 h-8 flex items-center ml-2">
         <nuxt-link class="back" :to="{ query: null }">&larr; Back</nuxt-link>
@@ -42,12 +43,9 @@
       >
         <IconAngleUp />
       </div> -->
-
     </div>
 
     <!-- force disabled as above dragging is triggered by unknown reasons -->
-
-
 
     <!-- <div
       class="absolute top-0 right-0 z-20 p-4 text-pink-500"
@@ -56,16 +54,22 @@
       {{ isDraggableInfoPanelDisabled ? "true" : "false" }}
     </div> -->
 
-
-
     <!-- <div>is mobile portrait: {{ isMobilePortrait ? "true" : "false" }}</div>
     -->
     <!-- <div>
       isDraggableInfoPanelCollapsed:
       {{ isDraggableInfoPanelCollapsed ? "true" : "false" }}
     </div> -->
-    <AppMapPanelSite class="absolute inset-0 top-8 z-10 p-4 bg-white" v-if="!building||true" :key="'site'" />
-    <AppMapPanelBuilding class="absolute inset-0 top-8 z-20 p-4 bg-white" v-if="building" :key="building.id" />
+    <AppMapPanelSite
+      class="absolute inset-0 top-8 z-10 p-4 bg-white"
+      v-if="!building || true"
+      :key="'site'"
+    />
+    <AppMapPanelBuilding
+      class="absolute inset-0 top-8 z-20 p-4 bg-white"
+      v-if="building"
+      :key="building.id"
+    />
   </div>
 </template>
 
@@ -76,6 +80,13 @@ import AppMapPanelBuilding from "~/components/map/AppMapPanelBuilding";
 import IconX from "~/components/icons/IconX.vue";
 import IconAngleUp from "~/components/icons/IconAngleUp.vue";
 import IconAngleDown from "~/components/icons/IconAngleDown.vue";
+
+import gsap from "gsap";
+import { ScrollToPlugin } from "gsap/dist/ScrollToPlugin.js";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger.js";
+gsap.registerPlugin(ScrollToPlugin);
+gsap.registerPlugin(ScrollTrigger);
+
 export default {
   data() {
     return {
@@ -128,6 +139,13 @@ export default {
     //     window.removeEventListener("scroll", self.onWindowScroll, false);
     //   }
     // },
+    scrollTo(y) {
+      // console.log(id)
+      if (process.client) {
+        // gsap.to(window, { duration: 0.5, scrollTo: { y: id, offsetY } });
+        gsap.to(window, { duration: 0.5, scrollTo: { y } });
+      }
+    },
     dragTriggerTouchStart() {
       this.$store.dispatch("map/setIsDraggableInfoPanelDisabled", {
         isDisabled: false
@@ -152,7 +170,7 @@ export default {
       //   });
       // });
       // }, 0);
-    },
+    }
     // expandDraggablePanel() {
     //   this.$store.dispatch("map/setIsDraggableInfoPanelExpanded", {
     //     isExpanded: false
@@ -192,6 +210,32 @@ export default {
     //     this.lastPanelScrollTop = scrollTop;
     //   }
     // }
+  },
+  watch: {
+    isDraggableInfoPanelCollapsed(newVal, oldVal) {
+      console.log("new is collapsed", newVal, "old is collapsed", oldVal);
+      if (newVal === true) {
+        // this.resetPanelHeightAndBound(0);
+        // gsap.to(".app-map-panel", { y: 0, duration: 0.3 });
+        // // this.draggable[0].enable();
+        // this.$store.dispatch("map/setIsDraggableInfoPanelDisabled", {
+        //   isDisabled: false
+        // });
+        // this.scrollTo('.map-content',0)
+        // this.scrollTo(0)
+        // document.querySelector('.map-content').scrollTop=0
+        gsap.to(".map-content", { scrollTop: 0 });
+      } else {
+        // this.resetPanelHeightAndBound(-this.initialPanelHeight);
+        // gsap.to(".app-map-panel", {
+        //   y: -this.initialPanelHeight,
+        //   duration: 0.3
+        // });
+        // this.$store.dispatch("map/setIsDraggableInfoPanelDisabled", {
+        //   isDisabled: true
+        // });
+      }
+    }
   }
 };
 </script>
