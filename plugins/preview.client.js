49f948857dff358c25d779e8625c6f9bd36111e3
Jason <jason@hato.co>
2021-08-23 16:39:50 +0100

    hide preview.client.js as it creates unknown enablePreview not found error

--- plugins/preview.client.js
+++ plugins/preview.client.js
@@ -1,5 +1,13 @@
 export default function ({ query, enablePreview }) {
+  console.log('query from preview.client.js', query, enablePreview)
   if (query.preview) {
     enablePreview()
   }
 }
+// // export default function ({ query, enablePreview }) {
+//   export default function (abc) {
+//   debugger    
+//   if (query.preview) {
+//     enablePreview()
+//   }
+// }
