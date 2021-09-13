aca87159d0dafa07767ea9a3e5cf00b8db83aeb9
jolc <jason@hato.co>
2021-09-13 16:15:41 +0100

    updated video block

--- components/blocks/BlockVideo.vue
+++ components/blocks/BlockVideo.vue
@@ -2,19 +2,35 @@
   <section class="block-video"
   v-if="block && block.fields"
   >
-    <div class="ratio-container">
+    <div 
+    class="ratio-container ratio-container-desktop hidden lg:block"
+    :style="{
+      paddingBottom: `${(1/videoRatio)*100}%`
+    }"
+    >
       <div class="iframe-wrapper">
         <iframe :src="this.videoSource" width="" height=""></iframe>
       </div>
     </div>
 
     <div 
+    class="ratio-container ratio-container-mobile block lg:hidden"
+    :style="{
+      paddingBottom: `${(1/videoRatioMobile)*100}%`
+    }"
+    >
+      <div class="iframe-wrapper">
+        <iframe :src="this.videoSourceMobile" width="" height=""></iframe>
+      </div>
+    </div>
+
+    <!-- <div 
     v-if="$route.path.indexOf('space-to-create')>0"
     @click="go" 
     class="full-screen-banner-action inset-x-0 bottom-0 m-4 text-center uppercase text-white z-10 transition transition-all duration-200 ease-in-out cursor-pointer transform hover:-translate-y-1" style="font-weight:500; letter-spacing:1px;">
       View Offer <br>
       ↓
-    </div>
+    </div> -->
 
   </section>
 </template>
@@ -39,6 +55,15 @@ export default {
     videoSource () {
       return this.block && this.block.fields.videoUrl || ''
     },
+    videoRatio () {
+      return this.block && this.block.fields.videoRatio || 16/9
+    },
+    videoSourceMobile () {
+      return this.block && this.block.fields.videoUrlMobile || this.block && this.block.fields.videoUrl || ''
+    },
+    videoRatioMobile () {
+      return this.block && this.block.fields.videoRatioMobile || this.block && this.block.fields.videoRatio || 16/9
+    },
     isIFrame () {
       return this.block && this.videoSource && _.includes(this.videoSource, 'player.vimeo')
     },
@@ -54,17 +79,19 @@ export default {
   overflow: hidden;
   position: relative;
   // height: 56vw;
-  height: 45vw;
-  @screen xl {
-    height: 40rem;
-  }
+  
+  
+  // height: 45vw;
+  // @screen xl {
+  //   height: 40rem;
+  // }
 
   .ratio-container {
     width: 100%;
     padding-bottom: 56.25%;
-    position: absolute;
-    top: 50%;
-    transform: translateY(-50%);
+    // position: absolute;
+    // top: 50%;
+    // transform: translateY(-50%);
   }
   .iframe-wrapper {
     display: block;
